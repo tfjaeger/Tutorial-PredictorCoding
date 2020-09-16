5d8f55116ef4f1ca596e10d1a2c66a56ba3409ae
tfjaeger <fjaeger@ur.rochester.edu>
2020-09-16 17:08:42 -0400

    imported full data and finalized first draft of complete tutorial.

--- scripts/load and prepare Clark data.R
+++ scripts/load and prepare Clark data.R
@@ -3,19 +3,25 @@ library(magrittr)
 library(R.matlab)
 
 # First let's get the MatLab structure
-s = readMat("Scripts/Week_3_Foundations/data_ClarkCrowding.mat")
+s = readMat("data/data_ClarkCrowding.mat")
 
-d.uncrowded = matrix(s$dataCrowding[[1]], nrow=dim(s$dataCrowding[[1]])[3], byrow = T) %>% 
+d.fixation = matrix(s$dataCrowding[[1]], nrow=dim(s$dataCrowding[[1]])[3], byrow = T) %>% 
+  as.data.frame() %>%
+  mutate(
+    Subject = 1:10,
+    Condition = "Fixation")
+d.uncrowded = matrix(s$dataCrowding[[2]], nrow=dim(s$dataCrowding[[2]])[3], byrow = T) %>% 
   as.data.frame() %>%
   mutate(
     Subject = 1:10,
     Condition = "Uncrowded")
-d.crowded = matrix(s$dataCrowding[[2]], nrow=dim(s$dataCrowding[[2]])[3], byrow = T) %>% 
+d.crowded = matrix(s$dataCrowding[[3]], nrow=dim(s$dataCrowding[[3]])[3], byrow = T) %>% 
   as.data.frame() %>%
   mutate(
     Subject = 1:10,
     Condition = "Crowded")
 d = rbind(
+  d.fixation,
   d.uncrowded,
   d.crowded)
 
@@ -24,8 +30,7 @@ d %<>%
   select(Subject, Condition, everything()) %>%
   unnest()
 
-write_csv(d, path = "Scripts/Week_3_Foundations/data_ClarkCrowding.csv")
-save(d, file = "Scripts/Week_3_Foundations/data_ClarkCrowding.Rdata")
+write_csv(d, path = "data/data_ClarkCrowding.csv")
 
 
 
